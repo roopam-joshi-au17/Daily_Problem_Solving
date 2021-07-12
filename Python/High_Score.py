1a53f19fc2fc6ce9089049d297783e0007f4d77c
roopam joshi <roopamjoshi63@gmail.com>
2021-07-12 18:04:30 +0530

    daily commit

---
+++ Python/High_Score.py
@@ -0,0 +1,10 @@
+student_scores=input("Input a list of student scores ").split()
+for n in range(0,len(student_scores)):
+    student_scores[n]=int(student_scores[n])
+print(student_scores)
+heighest_score=0
+for score in student_scores:
+    if score>heighest_score:
+        heighest_score=score
+print(f"Heighest score is :{heighest_score}")
+       
