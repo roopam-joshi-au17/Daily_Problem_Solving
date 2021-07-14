33b165effefab5067e40c859d5d60e5a62b02acd
roopam joshi <roopamjoshi63@gmail.com>
2021-07-14 09:42:06 +0530

    daily commit

---
+++ Python/Area_calculator.py
@@ -0,0 +1,9 @@
+import math
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+def paint_calc(height,width,cover):
+    area=height * width
+    number_of_cans = math.ceil(area / cover)
+    print(f"You will need")
+paint_calc(height=test_h, width=test_w, cover=coverage)
