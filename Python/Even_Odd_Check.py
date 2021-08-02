693615296eb9afc801a6738ce5da27a7705bbab9
roopam joshi <roopamjoshi63@gmail.com>
2021-08-02 18:56:46 +0530

    done

--- Python/Even_Odd_Check.py
+++ Python/Even_Odd_Check.py
@@ -1,5 +1,5 @@
 number=int(input("Which Number You Want To Check ? "))
 if number%2==0:
-    print(f"{number} is an Even Number")
+    print(f"{number} is an Even Number.")
 else:
-    print(f"{number} is an Odd Number")
+    print(f"{number} is an Odd Number.")
